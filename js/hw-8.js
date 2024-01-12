dd858e34932b4571c7ac4ac38909faf47300d18a
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-01-12 18:03:47 +0200

    .

---
+++ js/hw-8.js
@@ -0,0 +1,60 @@
+//№1
+//  var i = 1;
+
+//  while (i <= 10) {
+//    console.log(i);
+//    i++;
+//  }
+
+
+ //№2
+//  for (var i = 2; i <= 20; i += 2) {
+//     console.log(i);
+//   }
+
+
+ //№3
+ //опрацювати на менторській
+
+
+ //№4
+//  var numbersArray = [1, 2, 3, 4, 5];
+
+//  var i = 0;
+//  while (i < numbersArray.length) {
+//    console.log(numbersArray[i]);
+//    i++;
+//  }
+
+
+ //№5
+
+//  var numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//  for (var i = 0; i < numbersArray.length; i++) {
+//    if (numbersArray[i] === 7) {
+//      break;
+//    }
+//    console.log(numbersArray[i]);
+//  }
+
+
+ //№6
+
+//  var n = 7;
+//  for (var i = 1; i < n; i++) {
+//    console.log(i);
+//  }
+
+
+ //№7
+
+// var i = 1;
+//     while (i <= 20) {
+//       if (i % 3 === 0) {
+//         i++;
+//         continue;
+//       }
+//       console.log(i);
+//       i++;
+//     }
