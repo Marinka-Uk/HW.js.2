5e026b5de9b8c6e1991335f37474713fc6f56f26
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2024-01-13 23:55:26 +0200

    .

---
+++ js/hw-9.js
@@ -0,0 +1,67 @@
+//1.Створити масив з трьох чисел. Змінити значення другого елемента масиву на 10.
+
+let firstArray = [30, 40, 567];
+firstArray[1] = 10;
+console.log(firstArray);
+
+
+
+//2.Створити масив із трьох рядків. Додати до масиву ще одну рядків.
+
+let secondArray = ['44', '89', '1'];
+secondArray[3] = '2';
+console.log(secondArray);
+
+
+//3.Створити скрипт який поверне суму всіх чисел в масиві.
+
+var landArray = [3, 7, 12, 5, 8];
+function calculateSum(array) {
+    var sum = 0;
+  
+    for (var i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  
+    return sum;
+  }
+var totalSum = calculateSum(landArray);
+  console.log("Сума всіх чисел у масиві: " + totalSum);
+
+//4.Створити масив з 5-ти чисел. Вивести на екран всі елементи масиву за допомогою циклу for.
+
+var landArray = [2, 8, 5, 12, 7];
+for (var i = 0; i < landArray.length; i++) {
+  console.log("Елемент №" + (i + 1) + ": " + landArray[i]);
+}
+
+
+//5.Створити масив із 5-ти рядків. Вивести на екран кожен рядокз масиву, який містить більше 5-ти символів.
+
+const bothArray = ['dmdSDD', 'acijsc', 'sduh', 'sldjnAA', 'scns'];
+for (let i = 0; i < bothArray.length; i +=1){
+    if (bothArray[i].length > 5) {console.log(bothArray[i])} else {continue}
+}
+
+
+
+//6.Створити масив з 10-ти чисел. Знайти та вивести на екран максимальне значення з масиву.
+
+let numbersArray = [17, 5, 8, 21, 12, 4, 10, 14, 19, 6];
+let maxNumber = numbersArray[0];
+for (let i = 1; i < numbersArray.length; i++) {
+  if (numbersArray[i] > maxNumber) {
+    maxNumber = numbersArray[i];
+  }
+}console.log("Максимальне значення у масиві: " + maxNumber);
+
+
+//7.Створити масив з 10-ти чисел. Знайти всі парні числа в масиві та вивести їх на екран.
+
+let numberArray = [7, 12, 5, 8, 21, 14, 10, 19, 2, 6];
+
+for (let i = 0; i < numberArray.length; i++) {
+  if (numberArray[i] % 2 === 0) {
+    console.log("Парне число: " + numberArray[i]);
+  }
+}
